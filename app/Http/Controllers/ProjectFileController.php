87311caaee3082a419f35c9a1fa12dc4522fd6ac
GlobSecure Informatica Ltda <contato@globsecure.com.br>
2015-10-03 16:58:11 -0300

    Step 7 - Upload without Service next step add UploadService.

---
+++ app/Http/Controllers/ProjectFileController.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace GlobProject\Http\Controllers;
+
+use GlobProject\Repositories\ProjectRepository;
+use GlobProject\Services\ProjectService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
+
+class ProjectFileController extends Controller
+{
+
+    /**
+     * @var ProjectRepository
+     */
+    private $repository;
+    /**
+     * @var ProjectService
+     */
+    private $service;
+
+    /**
+     * @param ProjectRepository $repository
+     * @param ProjectService $service
+     */
+    public function __construct(ProjectRepository $repository, ProjectService $service) {
+        $this->repository = $repository;
+        $this->service = $service;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $userId = \Authorizer::getResourceOwnerId();
+        return $this->repository->findWhere(['owner_id' => $userId]);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $file = $request->file('file');
+        $extension = $file->getClientOriginalExtension();
+
+        Storage::put($request->name.".".$extension, File::get($file));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        if ($this->checkProjectPermissions($id) == false) {
+            return ['error' => 'Access Forbidden'];
+        }
+
+        return $this->repository->find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        if ($this->checkProjectPermissions($id) == false) {
+            return ['error' => 'Access Forbidden'];
+        }
+
+        return $this->service->update($request->all(), $id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        if ($this->checkProjectPermissions($id) == false) {
+            return ['error' => 'Access Forbidden'];
+        }
+
+        if ($client = $this->repository->find($id)) {
+            $client->delete();
+        }
+    }
+
+    /**
+     * @param $projectId
+     * @return array
+     */
+    private function checkProjectOwner($projectId)
+    {
+        $userId = \Authorizer::getResourceOwnerId();
+        return $this->repository->isOwner($projectId, $userId);
+    }
+
+    private function checkProjecMember($projectId)
+    {
+        $userId = \Authorizer::getResourceOwnerId();
+        return $this->repository->hasMember($projectId, $userId);
+    }
+
+    private function checkProjectPermissions($projectId)
+    {
+        if ($this->checkProjectOwner($projectId) or $this->checkProjecMember($projectId)) {
+            return true;
+        }
+        return false;
+    }
+}
